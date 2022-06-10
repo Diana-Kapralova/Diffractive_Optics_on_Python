7197ceab8ea3cd906779bf93b79ee0446e05b368
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-10 18:17:31 +0300

    Added solution to Exercise 1

--- 2.Design_of_Diffractive_Optical_Elements/Exercise/Ex1.py
+++ 2.Design_of_Diffractive_Optical_Elements/Exercise/Ex1.py
@@ -8,11 +8,12 @@ N = 500  # matrix size
 A = np.ones((1, N))  # Define amplitude matrices with amplitude vale = 1
 P = 100  # period of gratings
 FF = 0.5  # fill factor
+phase = 1.67  # Change phase parameter according to 0.5 changes of intensity
 
 for q in range(N):
     if np.remainder(q, P) < P*FF:
-        # Change phase parameter
-        A[0, q] = np.exp(1j*np.pi)
+        # Change phase parameter according to 0.5 changes of phase
+        A[0, q] = np.exp(1j*phase)
 
 A = np.tile(A, (N, 1))
 
