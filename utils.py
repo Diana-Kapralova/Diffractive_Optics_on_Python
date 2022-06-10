24fc2f7967aa0a2e9e034b3f117f30055c8a9076
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-10 16:41:33 +0300

    Added description for norm_output

--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def norm_output(x, N):
+    """
+    Evaluating field stregth E and it intensity IN.
+    Plot diffractive element and it distribution of
+    diffractive field
+
+    :param x: amplitude matrix
+    :param N: resolution of matrix
+    :return: plots of diffraction element
+    and distribution of diffractive field
+    """
     E = np.fft.fftshift(np.fft.fft2(x))
     IN = (abs(E)/(N*N) * abs(E)/(N*N))
     plt.figure(figsize=(6, 6))
