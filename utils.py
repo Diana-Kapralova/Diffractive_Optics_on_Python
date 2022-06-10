311dd5f57216a7935444e7480aea19cec9f7b1cd
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-10 17:29:10 +0300

    Added to norm_output  parameter angle for phase ploting

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def norm_output(x, N):
+def norm_output(x, N, angle=False):
     """
     Evaluating field stregth E and it intensity IN.
     Plot diffractive element and it distribution of
@@ -9,13 +9,17 @@ def norm_output(x, N):
 
     :param x: amplitude matrix
     :param N: resolution of matrix
+    :param angle: show phase component
     :return: plots of diffraction element
     and distribution of diffractive field
     """
     E = np.fft.fftshift(np.fft.fft2(x))
     IN = (abs(E)/(N*N) * abs(E)/(N*N))
     plt.figure(figsize=(6, 6))
-    plt.imshow(x, cmap='gray')
+    if angle:
+        plt.imshow(np.angle(x), cmap='gray')
+    else:
+        plt.imshow(x, cmap='gray')
     plt.figure(figsize=(6, 6))
     plt.imshow(IN, cmap='gray')
     plt.show()
