fd9996579adf667067a6241d7fd1f5af3031a207
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-10 16:34:45 +0300

    Added explanation comments

--- circular_fzp.py
+++ circular_fzp.py
@@ -12,13 +12,16 @@ lamb = 0.633  # wavelength in micrometers
 
 # Constracting FZP
 for n in range(M):
+    # equation for FZP lines
     r1[n] = np.sqrt(n*f*lamb)
 
 for n in range(0, M, 2):
     for p in range(N):
         for q in range(N):
+            # create polar system of coordinates
             r[p, q] = np.sqrt((p - N/2)*(p - N/2) + (q - N/2)*(q - N/2))
             if (np.all(r[p, q] > r1[n])) and (np.all(r[p, q] < r1[n+1])):
+                # change amplitude by phase(phase FZP)
                 A[p, q] = np.exp(1j*np.pi)
                 print(p, q)
     print(n)
