18a1c550635c327b6c15bdcd7aecda703d48b559
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-13 16:38:31 +0300

    Added complex type for phase implementation

--- 3.Advanced_Diffractive_Optical_Elements/blazed_1D_phase_grating.py
+++ 3.Advanced_Diffractive_Optical_Elements/blazed_1D_phase_grating.py
@@ -1,13 +1,16 @@
 import numpy as np
 from utils import plot_output
+import matplotlib.pyplot as plt
 
 # Defining gratings parameters
 N = 500
 P = 100
-A1 = np.ones(P, N)
+A1 = np.ones((P, N), dtype='complex_')
 
 for p in range(P):
     A1[p, :] = np.exp(1j * (p/P) * 2 * np.pi)
-
+plt.plot(A1)
 A2 = np.tile(A1, (int(N/P), 1))
+
+plot_output(A2, N, angle=True, profile=True)
 
