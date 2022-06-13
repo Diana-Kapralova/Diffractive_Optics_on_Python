58739b6517fcc9814bfea60c69829187c8c91dab
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-13 15:10:48 +0300

    Added phase profile parameter

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_output(x, N, angle=False):
+
+def plot_output(x, N, angle=False, profile=False):
     """
     Evaluating field stregth E and it intensity IN.
     Plot diffractive element and it distribution of
@@ -10,8 +11,9 @@ def plot_output(x, N, angle=False):
     :param x: amplitude matrix
     :param N: resolution of matrix
     :param angle: show phase component
-    :return: plots of diffraction element
-    and distribution of diffractive field
+    :param profile: show phase profile
+    :return: plots of diffraction element,
+    distribution of diffractive field, phase profile
     """
     E = np.fft.fftshift(np.fft.fft2(x))
     IN = (abs(E)/(N*N) * abs(E)/(N*N))
@@ -22,4 +24,11 @@ def plot_output(x, N, angle=False):
         plt.imshow(x, cmap='gray')
     plt.figure(figsize=(6, 6))
     plt.imshow(IN, cmap='gray')
+
+    if profile:
+        # Plot phase profile
+        plt.figure(figsize=(6, 6))
+        plt.plot(np.angle(x[:, int(N / 2)]))
+        plt.xlabel('Profile pixels')
+        plt.ylabel('Phase (radiance)')
     plt.show()
