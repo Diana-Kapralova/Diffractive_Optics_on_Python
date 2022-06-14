62b6e3880142f995198ea04aba740773c09077ad
Diana Kapralova <kapralova.d.s@gmail.com>
2022-06-14 21:06:51 +0300

    Added spiral parameter

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_output(x, N, angle=False, profile=False):
+def plot_output(x, N, angle=False, profile=False, spiral=False):
     """
     Evaluating field stregth E and it intensity IN.
     Plot diffractive element and it distribution of
@@ -12,11 +12,15 @@ def plot_output(x, N, angle=False, profile=False):
     :param N: resolution of matrix
     :param angle: show phase component
     :param profile: show phase profile
+    :param spiral: calculate field that not depend of N for SPP
     :return: plots of diffraction element,
     distribution of diffractive field, phase profile
     """
     E = np.fft.fftshift(np.fft.fft2(x))
-    IN = (abs(E)/(N*N) * abs(E)/(N*N))
+    if spiral:
+        IN = abs(E) * abs(E)
+    else:
+        IN = (abs(E)/(N*N) * abs(E)/(N*N))
     plt.figure(figsize=(6, 6))
     if angle:
         plt.imshow(np.angle(x), cmap='gray')
